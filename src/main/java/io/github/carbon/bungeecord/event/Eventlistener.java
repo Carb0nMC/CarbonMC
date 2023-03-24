85a6682023fa02b9b682b7ea676a6032bfc9d7b6
Carb0nMC <carbon@carbonmc.net>
2023-03-24 20:59:25 +0100

    Added Feature to prevent Players to join while maintenance

--- src/main/java/io/github/carbon/bungeecord/event/Eventlistener.java
+++ src/main/java/io/github/carbon/bungeecord/event/Eventlistener.java
@@ -6,7 +6,6 @@ import io.github.carbon.carbonmc.utils.file.FileManager;
 import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PostLoginEvent;
-import net.md_5.bungee.api.event.PreLoginEvent;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
